0fbf2ac9792d6eb5d4b72ac66016c077bbfdd6c7
Filip Mihajlovski <fitz993m@gmail.com>
2020-12-18 15:49:14 +0100

    1.13. Footer

--- components/IndexMenu/IndexMenu.js
+++ components/IndexMenu/IndexMenu.js
@@ -12,22 +12,26 @@ const IndexMenu = () => {
         <h4>JavaScript Developer</h4>
       </div>
       <div className={styles.indexIcons}>
-        <IconButton>
+        <IconButton className={styles.indexButton}>
           <a href="http://facebook.com">
             <FacebookIcon className={styles.fbIcon} />
           </a>
         </IconButton>
-        <IconButton>
+        <IconButton className={styles.indexButton}>
           <a href="https://linkedin.com/in/filip-mihajlovski-68773116b">
             <LinkedInIcon className={styles.linkedIcon} />
           </a>
         </IconButton>
-        <IconButton>
+        <IconButton className={styles.indexButton}>
           <a href="https://github.com/philip993?tab=repositories">
             <GitHubIcon className={styles.gitIcon} />
           </a>
         </IconButton>
       </div>
+      <footer className={styles.footer}>
+        <h6>Copyright &#169; 2020</h6>
+        <h6>All Rights Reserved</h6>
+      </footer>
     </div>
   );
 };
