f8de7137129cab8c1835101ef492cc7d957caf3c
Filip FICO <fitz993m@gmail.com>
2020-12-19 23:39:01 +0100

    1.15. Projects page completed

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,5 @@
-import Layout from "../components/Layout/Layout";
-import styles from "../styles/projects.module.scss";
+import Layout from '../components/Layout/Layout';
+import styles from '../styles/projects.module.scss';
 import {
   Card,
   CardHeader,
@@ -7,13 +7,15 @@ import {
   CardActionArea,
   Button,
   CardActions,
-} from "@material-ui/core";
+} from '@material-ui/core';
 
-import ReactLogo from "../public/react-js.svg";
-import ExpressLogo from "../public/expressjs.svg";
-import MongoLogo from "../public/mongodb.svg";
-import SassLogo from "../public/sass.svg";
-import NodeLogo from "../public/node-js.svg";
+import ReactLogo from '../public/react-js.svg';
+import ExpressLogo from '../public/expressjs.svg';
+import MongoLogo from '../public/mongodb.svg';
+import SassLogo from '../public/sass.svg';
+import NodeLogo from '../public/nodejs.svg';
+import MySqlLogo from '../public/mysql.svg';
+import NestLogo from '../public/nestjs_logo.svg';
 
 const Projects = () => (
   <div className={styles.projects}>
@@ -24,133 +26,142 @@ const Projects = () => (
     <div className={styles.body}>
       <section className={styles.sectionOne}>
         <Card className={styles.card}>
-          <h2>E-COMMERCE</h2>
-          <CardContent>
-            <ul>
-              <h5>STACK</h5>
-              <li>
+          <div className={styles.cardHeader}>
+            <h2>E-COMMERCE</h2>
+          </div>
+          <CardContent className={styles.content}>
+            <div className={styles.cardSpan}>
+              <div className={styles.buttonContainer}>
+                <Button className={styles.btn}>
+                  <a href="https://github.com/philip993/e-com">GitHub</a>
+                </Button>
+              </div>
+              <p>
+                E-Commerce website. Provides User Sign-Up/Login with
+                authentication system. Logged User can browse through Shop. User
+                can create Order, view or pay the any Order on his account.
+                Implemented payment system - STRAPI.
+              </p>
+            </div>
+
+            <div className={styles.cardFooter}>
+              <div className={styles.imageContrainer}>
                 <img className={styles.images} src={MongoLogo} alt="" />
-              </li>
-              <li>
+              </div>
+              <div className={styles.imageContrainer}>
                 <img className={styles.images} src={ExpressLogo} alt="" />
-              </li>
-              <li>
+              </div>
+              <div className={styles.imageContrainer}>
                 <img className={styles.images} src={ReactLogo} alt="" />
-              </li>
-              <li>
+              </div>
+              <div className={styles.imageContrainer}>
                 <img className={styles.images} src={NodeLogo} alt="" />
-              </li>
-            </ul>
-            <div className={styles.cardSpan}>
-              <h5>Description</h5>
-              <p disabled>
-                E-Commerce website. Provides User Sign-Up/Login with
-                authentication system. Logged User can browse through Shop. User
-                can select Book and add into Cart. Can clear all Cart or any
-                selected Book. User can create Order, view or pay the any Order
-                on his account. Implemented payment system - STRAPI. Added SKU
-                for Books.
-              </p>
+              </div>
             </div>
           </CardContent>
-          <div className={styles.btnArea}>
-            <Button className={styles.btn}>
-              <a href="https://github.com/philip993/e-com">GitHub</a>
-            </Button>
-          </div>
-        </Card>
-        <Card className={styles.card}>
-          <h2>E-COMMERCE</h2>
-          <CardContent>
-            <ul>
-              <h5>STACK</h5>
-              <li>MongoDB</li>
-              <li>Express</li>
-              <li>React</li>
-              <li>Node</li>
-            </ul>
-            <div className={styles.cardSpan}>
-              <h5>Description</h5>
-              <p disabled>
-                E-Commerce website. Provides User Sign-Up/Login with
-                authentication system. Logged User can browse through Shop. User
-                can select Book and add into Cart. Can clear all Cart or any
-                selected Book. User can create Order, view or pay the any Order
-                on his account. Implemented payment system - STRAPI. Added SKU
-                for Books.
-              </p>
-            </div>
-          </CardContent>
-          <div className={styles.btnArea}>
-            <Button className={styles.btn}>
-              <a href="https://github.com/philip993/e-com">GitHub</a>
-            </Button>
-          </div>
         </Card>
       </section>
 
       <section className={styles.sectionTwo}>
         <Card className={styles.card}>
-          <h2>EMPLOYEE RECORDS</h2>
-          <CardContent>
-            <ul>
-              <h5>STACK</h5>
-              <li>MySQL</li>
-              <li>Express</li>
-              <li>React</li>
-              <li>Node</li>
-            </ul>
+          <div className={styles.cardHeader}>
+            <h2>EMPLOYEE RECORDS</h2>
+          </div>
+          <CardContent className={styles.content}>
             <div className={styles.cardSpan}>
-              <h5>Description</h5>
+              <div className={styles.buttonContainer}>
+                <Button className={styles.btn}>
+                  <a href="https://github.com/philip993/employee-rec">GitHub</a>
+                </Button>
+              </div>
               <p>
-                Employee Records is HR application. First need to add Employee
-                in the system in the registration form. Next you can add
-                Contract to the Employee. You can check for days left of any
-                contract, as well as update or terminate contract. Option to
-                send Emails for extentions or termination of contract. Next you
-                need to insert Employee into Shift schedule. Option to change
-                status from active to inactive or change Shift. Automatically
-                show number of Meals per shift and total Meals. Lists for
-                Inactive and Fired Employees.
+                Employee Records is application suitable for HR management.
+                Allows total User evidention and various schedules. Option to
+                create PDF documents, such as Contracts, Contract Extentions and
+                Terminations. Added option to send mail and attach documents.
               </p>
             </div>
+            <div className={styles.cardFooter}>
+              <div className={styles.imageContrainer}>
+                <img className={styles.images} src={MySqlLogo} alt="" />
+              </div>
+              <div className={styles.imageContrainer}>
+                <img className={styles.images} src={ExpressLogo} alt="" />
+              </div>
+              <div className={styles.imageContrainer}>
+                <img className={styles.images} src={ReactLogo} alt="" />
+              </div>
+              <div className={styles.imageContrainer}>
+                <img className={styles.images} src={NodeLogo} alt="" />
+              </div>
+            </div>
           </CardContent>
-          <div className={styles.btnArea}>
-            <Button className={styles.btn}>
-              <a href="https://github.com/philip993/employee-rec">GitHub</a>
-            </Button>
-          </div>
         </Card>
+      </section>
+
+      <section className={styles.sectionThree}>
         <Card className={styles.card}>
-          <h2>EMPLOYEE RECORDS</h2>
-          <CardContent>
-            <ul>
-              <h5>STACK</h5>
-              <li>MySQL</li>
-              <li>Express</li>
-              <li>React</li>
-              <li>Node</li>
-            </ul>
+          <div className={styles.cardHeader}>
+            <h2>ACCOUNTING APP</h2>
+          </div>
+          <CardContent className={styles.content}>
             <div className={styles.cardSpan}>
-              <h5>Description</h5>
+              <div className={styles.buttonContainer}>
+                <Button className={styles.btn}>
+                  <a href="https://github.com/philip993/accounting">GitHub</a>
+                </Button>
+              </div>
               <p>
-                Employee Records is HR application. First need to add Employee
-                in the system in the registration form. Next you can add
-                Contract to the Employee. You can check for days left of any
-                contract, as well as update or terminate contract. Option to
-                send Emails for extentions or termination of contract. Next you
-                need to insert Employee into Shift schedule. Option to change
-                status from active to inactive or change Shift. Automatically
-                show number of Meals per shift and total Meals. Lists for
-                Inactive and Fired Employees.
+                Accounting App. Provides User with simple accounting software.
+                User can create Customers, Vendors, Invoices, etc. Various
+                statitics and reports.
               </p>
             </div>
+            <div className={styles.cardFooter}>
+              <div className={styles.imageContrainer}>
+                <img className={styles.images} src={MySqlLogo} alt="" />
+              </div>
+              <div className={styles.imageContrainer}>
+                <img className={styles.images} src={NestLogo} alt="" />
+              </div>
+              <div className={styles.imageContrainer}>
+                <img className={styles.images} src={ReactLogo} alt="" />
+              </div>
+              <div className={styles.imageContrainer}>
+                <img className={styles.images} src={NodeLogo} alt="" />
+              </div>
+            </div>
           </CardContent>
-          <div className={styles.btnArea}>
-            <Button className={styles.btn}>
-              <a href="https://github.com/philip993/employee-rec">GitHub</a>
-            </Button>
+        </Card>
+      </section>
+
+      <section className={styles.sectionFour}>
+        <Card className={styles.card}>
+          <div className={styles.cardHeader}>
+            <h2>OSM MAPS</h2>
           </div>
+          <CardContent className={styles.content}>
+            <div className={styles.cardSpan}>
+              <div className={styles.buttonContainer}>
+                <Button className={styles.btn}>
+                  <a href="https://github.com/philip993/osm-maps">GitHub</a>
+                </Button>
+              </div>
+              <p>
+                OSM-Maps is here to help you with the search for any desire
+                place. Get directions from your point to any point on the Map.
+                Search for Place of Interest around any given place on the Map.
+              </p>
+            </div>
+            <div className={styles.cardFooter}>
+              <div className={styles.imageContrainer}>
+                <img className={styles.images} src={ReactLogo} alt="" />
+              </div>
+              <div className={styles.imageContrainer}>
+                <img className={styles.images} src={SassLogo} alt="" />
+              </div>
+            </div>
+          </CardContent>
         </Card>
       </section>
     </div>
