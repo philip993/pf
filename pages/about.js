b864c649c7ac0cefee75a9d7b4433c01b34e5edd
Filip FICO <fitz993m@gmail.com>
2020-12-20 22:36:59 +0100

    1.20. About page

--- pages/about.js
+++ pages/about.js
@@ -2,64 +2,91 @@ import Layout from '../components/Layout/Layout';
 import styles from '../styles/about.module.scss';
 import { Avatar, Button } from '@material-ui/core';
 import Link from 'next/link';
+import CvButton from '../components/CvButton/CvButton';
 
-const About = () => (
-  <div className={styles.about}>
-    <Layout className={styles.t} />
-    <header className={styles.header}>
-      <h1>About Me</h1>
-    </header>
-    <div className={styles.body}>
-      <section>
-        <Avatar
-          className={styles.avatar}
-          alt="profie"
-          src={require('../public/pf.png')}
-        />
-        <span>
-          <h2>BIO</h2>
-          <p>
-            My name is Filip Mihajlovski. I'm 27 years old. Based in Kumanovo,
-            N. Macedonia.
-          </p>
-          <p>
-            I'm Full Stack JavaScript Developer. Prefer working with MERN stack.
-          </p>
-          <p>Follow the link to view or download my CV</p>
-          <p>
-            <Link href="/cv">
-              <a>Get CV</a>
-            </Link>
-          </p>
-        </span>
-      </section>
-      <div className={styles.info}>
-        <section>
-          <ul>
+const About = () => {
+  return (
+    <div className={styles.about}>
+      <Layout className={styles.t} />
+      <header className={styles.header}>
+        <h1>About Me</h1>
+      </header>
+      <div className={styles.body}>
+        <div className={styles.aboutContainer}>
+          <div className={styles.avatarContainer}>
+            <Avatar
+              className={styles.avatar}
+              alt="profie"
+              src={require('../public/pf.png')}
+            />
+            <CvButton />
+          </div>
+
+          <div className={styles.bContainer}>
+            <h2>BIO</h2>
+            <div className={styles.bioContainer}>
+              <p>
+                My name is Filip Mihajlovski. I'm 27 years old. Based in
+                Kumanovo, N. Macedonia.
+              </p>
+              <p>
+                I'm aspiring Full Stack JavaScript Developer. I prefer working
+                with MERN stack.
+              </p>
+              <p>
+                {/* Coming from management and financial accounting background with
+                eye for details and business know-how. */}
+                Coming from financial accounting field and management background
+                is giving me a knack for details and know-how from the industry.
+              </p>
+              <p>
+                I'm passionate about creating and delivering modern and
+                functional websites and applications.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.cvContainer}></div>
+        <div className={styles.info}>
+          <section>
             <h2>Frameworks/Libraries</h2>
-            <li>Proficient with React and Express</li>
-            <li>Used Angular and Handlebars</li>
-            <li>REST API</li>
-          </ul>
-        </section>
-        <section>
-          <ul>
+            <ul>
+              <li>Proficient with React and Express.</li>
+              <li>
+                Experience with NestJs, Next.js, AngularJs and Handlebars.
+              </li>
+              <li>REST API</li>
+            </ul>
+          </section>
+          <section>
             <h2>Databases</h2>
-            <li>MongoDB from the NoSQL Databases.</li>
-            <li>MySQL from SQL Databases.</li>
-          </ul>
-        </section>
-        <section>
-          <ul>
+            <ul>
+              <li>MongoDB with Mongoose.</li>
+              <li>MySQL with Sequelize and TypeORM.</li>
+            </ul>
+          </section>
+          <section>
             <h2>Features</h2>
-            <li>Implementation of Payment systems for E-commerce.</li>
-            <li>Adding Admin Panel to websites.</li>
-            <li>Authentication and Authorization</li>
-          </ul>
-        </section>
+            <ul>
+              <li>Implementation of Payment systems for E-commerce.</li>
+              <li>Adding Admin Panel to websites.</li>
+              <li>Authentication and Authorization.</li>
+              <li>Create UI for external API.</li>
+            </ul>
+          </section>
+          <section>
+            <h2>Tools</h2>
+            <ul>
+              <li>
+                Creating Mockups and high-fidelity Prototypes with Figma and
+                InVision Studio.
+              </li>
+              <li>Scheduling Project features and time-frames in Trello.</li>
+            </ul>
+          </section>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default About;
